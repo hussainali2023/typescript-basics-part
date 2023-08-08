2f7cc70593809bd57bc91cf0ced60c260f120572
Hussain <iamhussainali2@gmail.com>
2023-08-08 21:50:17 +0530

    added objects

--- src/array.ts
+++ src/array.ts
@@ -1,5 +1,10 @@
 const names: string[] = ["salam", "kalam", "palam"]
 let number: number[] = [32, 43, 50]
 
+
 number[8] = 80;
 console.log(number);
+
+let habiJabi = ["salam", "kalam", "palam", 32, 43, true, false, undefined, null]
+
+const user : [number, string] = [32, "abul"]  
---
+++ src/object.ts
@@ -0,0 +1,9 @@
+const user1: {
+    name: string,
+    age: number,
+    isMarried: boolean
+} = {
+    name: "Santu Mia",
+    age: 21,
+    isMarried: false
+}
